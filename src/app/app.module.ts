57a7a9b7db0caa48052d676e0e9f6dc19ec6f420
Johann Montfort <jmonfu@gmail.com>
2016-09-25 08:48:17 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './player-list/player-list.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerListComponent,
+    PlayerDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/player-list/player-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { PlayerListComponent } from './player-list.component';
+
+describe('Component: PlayerList', () => {
+  it('should create an instance', () => {
+    let component = new PlayerListComponent();
+    expect(component).toBeTruthy();
+  });
+});
